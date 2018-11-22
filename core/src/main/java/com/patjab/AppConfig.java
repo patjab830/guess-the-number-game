57563915cfb8d2b96ee9160be99e87f558f75505
Patrick Abejar <wireless.patrick@gmail.com>
2018-11-21 22:29:28 -0500

    switched to java configuration annotation classes

---
+++ core/src/main/java/com/patjab/AppConfig.java
@@ -0,0 +1,9 @@
+package com.patjab;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.patjab")
+public class AppConfig {
+}
--- core/src/main/java/com/patjab/Main.java
+++ core/src/main/java/com/patjab/Main.java
@@ -3,19 +3,20 @@ package com.patjab;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
+//    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
         // create the context (container)
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+//        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // get number generator bean from context
         NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
