303e40242e3177005fec3e0388d9d75808e08be7
Patrick Abejar <wireless.patrick@gmail.com>
2018-11-22 15:45:35 -0500

    used a properties file to assign max number and values

--- core/src/main/java/com/patjab/config/GameConfig.java
+++ core/src/main/java/com/patjab/config/GameConfig.java
@@ -1,16 +1,21 @@
 package com.patjab.config;
 
 import com.patjab.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
 
-    private int guessCount = 8;
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
